a2dc998dfba2ad108d436c4ee3502e007b66b3bd
Matias Galvan Guerrero <matias.galvan@tecsup.edu.pe>
2026-06-29 11:54:29 -0500

    fix: smtp puerto 587 para render

--- backend/src/controllers/newsletter.controller.js
+++ backend/src/controllers/newsletter.controller.js
@@ -2,10 +2,15 @@ const db = require('../db/db');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
